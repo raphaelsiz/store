af59713c6e9991ebd67e45e4bdfeab86aa7732e0
Raphael Morgan <rmorgansizemore@gmail.com>
2020-10-21 09:22:40 +0000

    added lining

--- script.js
+++ script.js
@@ -7,22 +7,26 @@ function testing() {
   if (typeSelect.value == "surgical") price = 2.5;
   else price = 5;
   var outsideSelect = document.getElementById("outside");
+  var liningSelect = document.getElementById("lining");
+  if (outsideSelect.value == "khaki" || outsideSelect.value == "green-khaki" || liningSelect == "khaki" || liningSelect == "green-khaki") price += 2.5;
   var sizeSelect = document.getElementById("size");
   var outsideValue = outsideSelect.options[outsideSelect.selectedIndex].text;
+  var liningValue = liningSelect.options[liningSelect.selectedIndex].text;
   var sizeValue = sizeSelect.options[sizeSelect.selectedIndex].text;
   var specialInstructions = document.getElementById("special").value;
   var string1 = "Outside fabric: ";
-  var string2 = ". Size: ";
-  var string3, string4;
+  var string2 = ". Lining: "
+  var string3 = ". Size: ";
+  var string4, string5;
   if (specialInstructions == "" || specialInstructions == " "){
-    string3 = "";
-    string4 = "."
+    string4 = "";
+    string5 = "."
   }
   else {
-    string3 = " (Special Instructions: ";
-    string4 = ")."
+    string4 = " (Special Instructions: ";
+    string5 = ")."
   }
-  var order = string1.concat(outsideValue,string2,sizeValue,string3,specialInstructions,string4);
+  var order = string1.concat(outsideValue,string2,liningValue,string3,sizeValue,string4,specialInstructions,string5);
   emailjs.send("service_7kldg0k","template_byjn2ur",{
     message: order,
   });
